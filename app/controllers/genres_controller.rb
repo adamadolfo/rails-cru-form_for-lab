4f63e7e5d5e41fffe10231fd169ccaf20175e221
Adam <adamadolfo8@gmail.com>
2020-09-17 16:26:56 -0400

    collab with alex

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,12 @@
+class GenresController < ApplicationController
+    def index
+        @genres = Genre.all
+    end
+    def show
+        @genre = Genre.find(params[:id])
+    end
+    def create
+    end
+    def new
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+drake = Artist.create(name: "Drake", bio: "Degrassi")
+taylor = Artist.create(name: "Taylor Swift", bio: "Cheesy")
+
+
+rap = Genre.find_or_create_by(name: "rap")
+pop = Genre.find_or_create_by(name: "pop")
+
+Song.destroy_all
+feel = Song.create(name: "feelings", artist_id: drake.id, genre_id: rap.id)
+tear_drops = Song.create(name: "tear drops on my guitar", artist_id: taylor.id, genre_id: pop.id)
